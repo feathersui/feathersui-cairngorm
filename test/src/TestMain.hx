50febd35198a4564df06501bcff55aeba2467f45
Josh Tynjala <joshtynjala@bowlerhat.dev>
2022-03-08 13:36:39 -0800

    test: html5

--- test/src/TestMain.hx
+++ test/src/TestMain.hx
@@ -14,10 +14,32 @@ class TestMain extends Sprite {
 		runner.addCase(new com.adobe.cairngorm.control.TestFrontController());
 		runner.addCase(new com.adobe.cairngorm.view.TestViewLocator());
 
+		#if (html5 && playwright)
+		// special case: see below for details
+		setupHeadlessMode(runner);
+		#else
 		// a report prints the final results after all tests have run
 		Report.create(runner);
+		#end
 
 		// don't forget to start the runner
 		runner.run();
 	}
+
+	#if (html5 && playwright)
+	/**
+		Developers using continuous integration might want to run the html5
+		target in a "headless" browser using playwright. To do that, add
+		-Dplaywright to your command line options when building.
+
+		@see https://playwright.dev
+	**/
+	private function setupHeadlessMode(runner:Runner):Void {
+		new utest.ui.text.PrintReport(runner);
+		var aggregator = new utest.ui.common.ResultAggregator(runner, true);
+		aggregator.onComplete.add(function(result:utest.ui.common.PackageResult):Void {
+			Reflect.setField(js.Lib.global, "utestResult", result);
+		});
+	}
+	#end
 }
